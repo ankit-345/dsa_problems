03e8d51ac24d58a6d07cf50091a472379af81d8b
ankit-345 <109626032+ankit-345@users.noreply.github.com>
2022-11-20 12:14:57 +0530

    Update reverse_k_nodes_in_linked_list.cpp
    New method updated

--- reverse_k_nodes_in_linked_list/reverse_k_nodes_in_linked_list.cpp
+++ reverse_k_nodes_in_linked_list/reverse_k_nodes_in_linked_list.cpp
@@ -54,7 +54,35 @@ node* reverse_k_groupLL(node* &head, int k){
     return prevptr;
 }
 
+ node* reverse_group(node* head, int k){
+    if(head == NULL || k == 1){
+        return head;
+    }
+    node *dummy = new node(0);
+    dummy->next = head;
+    node *pre = dummy, *curr = dummy, *nex = dummy;
 
+    int count = 0;
+    while(curr->next != NULL){
+        curr = curr->next;
+        count++;
+    }
+
+    while(count >= k){
+        curr = pre->next;
+        nex = curr->next;
+
+        for(int i=1; i<k; i++){
+            curr->next = nex->next;
+            nex->next = pre->next;
+            pre->next = nex;
+            nex = curr->next;
+        }
+        pre = curr;
+        count -= k;
+    }
+    return dummy;
+    }
 
 // ---------------------------------------------------------------------------------------------->
 void display(node *head)
@@ -77,9 +105,14 @@ int main()
     insertionTail(head, 4);
     insertionTail(head, 5);
     insertionTail(head, 6);
+    insertionTail(head, 7);
+    insertionTail(head, 8);
     display(head);
 
     head = reverse_k_groupLL(head, 3);
+    display(head);
+    
+    head = reverse_group(head, 3);
     display(head);
 
     return 0;
